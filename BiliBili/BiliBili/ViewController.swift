ded21982a57cd76ec6b8792e88ee6eb4c2777769
NearMilk <yzy153063616@qq.com>
2018-11-14 17:07:10 +0800

    Initialization.

---
+++ BiliBili/BiliBili/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiliBili
+//
+//  Created by administrator on 2018/11/14.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
