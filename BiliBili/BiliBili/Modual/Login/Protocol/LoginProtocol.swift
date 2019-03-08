e46ab1c090e67754868f69f03e1f8cb501307aaf
NearMilk <yzy153063616@qq.com>
2019-03-08 17:05:23 +0800

    add viper

---
+++ BiliBili/BiliBili/Modual/Login/Protocol/LoginProtocol.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewProtocol.swift
+//  BiliBili
+//
+//  Created by administrator on 2019/3/8.
+//  Copyright © 2019年 Yijo. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - View
+
+// Presenter -> View
+protocol LoginViewProtocol: class {
+    var presenter: LoginPresenterProtocol? { get set }
+}
+
+// MARK: - Router
+
+// Presenter -> Router
+protocol LoginRouterProtocol {
+    static func createLoginModule() -> UIViewController
+    
+    func presentAddLoginScreen(from view: LoginViewProtocol)
+}
+
+// MARK: - Presenter
+
+// View -> Presenter
+protocol LoginPresenterProtocol {
+    var view: LoginViewProtocol? { get set }
+    var interactor: LoginInteractorInputProtocol? { get set }
+    var wireFrame: LoginRouterProtocol? { get set }
+    func viewDidLoad()
+}
+
+// MARK: - Interactor
+
+// Presenter -> Interactor
+protocol LoginInteractorInputProtocol {
+    var presenter: LoginInteractorOutputProtocol? { get set }
+    var localDatamanager: LoginLocalDataManagerInputProtocol? { get set }
+}
+
+// Interactor -> Presenter
+protocol LoginInteractorOutputProtocol {
+    
+}
+
+
+
+// MARK: - DataManager
+
+// Interactor -> LocalDataManager
+protocol LoginLocalDataManagerInputProtocol {
+    
+}
---
+++ BiliBili/BiliBili/Modual/Login/Router/LoginRouter.swift
@@ -0,0 +1,15 @@
+//
+//  LoginRouter.swift
+//  BiliBili
+//
+//  Created by administrator on 2019/3/8.
+//  Copyright © 2019年 Yijo. All rights reserved.
+//
+
+class LoginRouter: LoginRouterProtocol {
+    
+    
+    func presentAddLoginScreen(from view: LoginViewProtocol) {
+        
+    }
+}
